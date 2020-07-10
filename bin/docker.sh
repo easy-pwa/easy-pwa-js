cfd35fce3037030e3b5dceb03069d051298014fb
Adrien Peyre <adrien-peyre@live.fr>
2020-07-10 15:54:05 +0200

    update

--- bin/docker.sh
+++ bin/docker.sh
@@ -2,4 +2,11 @@
 
 DIRECTORY=$(dirname $(realpath $0 ))
 
-docker run -it --rm --name easy-pwa-js -v "$DIRECTORY/..":/home/node/app -w /home/node/app -p 8080:8080 node:13 bash
+docker run -it --rm \
+  --name easy-pwa-js \
+  -v "$DIRECTORY/..":/home/node/app \
+  -w /home/node/app \
+  -p 8080:8080 \
+  -u $(id -u ${USER}):$(id -g ${USER}) \
+  node:14 \
+  bash
