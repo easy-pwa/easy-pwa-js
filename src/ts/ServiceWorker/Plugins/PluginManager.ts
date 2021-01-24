e56328017c5d93250db04fef035e1d532292f5f3
Adrien Peyre <adrien-peyre@live.fr>
2021-01-24 12:03:39 +0100

    service worker plugins

--- src/ts/ServiceWorker/Plugins/PluginManager.ts
+++
@@ -1,8 +0,0 @@
-import PageSubstitutionPlugin from './SubstitutionPage';
-
-export default class PluginManager {
-
-  public pageSubstitutionPlugin(): PageSubstitutionPlugin {
-    return new PageSubstitutionPlugin();
-  }
-}
